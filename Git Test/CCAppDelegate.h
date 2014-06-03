187ece7e5e20a0ea99b4f8fc4aae3a1435ae99ff
Jwill <johnwgarcia@gmail.com>
2014-06-02 22:50:41 -0400

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by johnny on 6/2/14.
+//  Copyright (c) 2014 johnwgarcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
