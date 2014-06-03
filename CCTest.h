1dc8bcd1feff5d359bfb82cde7df93c4c1a09a48
Jwill <johnwgarcia@gmail.com>
2014-06-02 23:20:49 -0400

    added a test class.

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by johnny on 6/2/14.
+//  Copyright (c) 2014 johnwgarcia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by johnny on 6/2/14.
+//  Copyright (c) 2014 johnwgarcia. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
